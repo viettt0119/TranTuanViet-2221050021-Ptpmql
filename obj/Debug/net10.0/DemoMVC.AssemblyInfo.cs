ad1422c82adc9ad488d1424d6dd09b5e3009a04b
viettt0119 <viettt0119@gmail.com>
2026-08-13 13:59:08 +0700

    stop and deloy sec2

--- obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8306a5b2147cc352b4afd5f92274a809344148cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce256015efaaff5a1ec283c7a279eef5b298d22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
