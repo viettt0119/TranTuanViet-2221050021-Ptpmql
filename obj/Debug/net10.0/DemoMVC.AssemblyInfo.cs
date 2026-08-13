3b15ed16c9ce87dc5cda5a29483e6db5eff0d61a
viettt0119 <viettt0119@gmail.com>
2026-08-13 14:20:35 +0700

    fix publish

--- obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce256015efaaff5a1ec283c7a279eef5b298d22b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6262bd6c19da2df9e9936aac955adba144378d77")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
