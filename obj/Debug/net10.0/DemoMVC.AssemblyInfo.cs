2f00efd56df228d0e099d19e080960c718382374
viettt0119 <viettt0119@gmail.com>
2026-09-24 14:20:18 +0700

    update database of product

--- obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d7993f2bbf600817996bc52582f043c1f760353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42968fcd9dfa7104229a146d962c4c734777713c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
